9417cbc0306c845c67f66b80f1435859ebded170
FaisalMS <fis445@hotmail.com>
2022-08-02 12:28:05 +0300

    second commit

---
+++ Main.kt
@@ -0,0 +1,39 @@
+fun main() {
+    val candles = 24
+    val layers = 5
+    println("Number of Candles: ${candles}")
+    println("Cake top length: ${candles}")
+    println("Number of layers: ${layers}")
+    printCakeCandles(candles)
+    printCakeTop(candles)
+    printCakeBottom(candles, layers)
+}
+
+fun printCakeCandles(candles: Int) {
+    print (" ")
+    repeat(candles) {
+        print(",")
+    }
+    println()
+    print(" ")
+    repeat(candles) {
+        print("|")
+    }
+    println()
+}
+
+fun printCakeTop(candles: Int) {
+    repeat(candles + 2) {
+        print("=")
+    }
+    println()
+}
+
+fun printCakeBottom(candles: Int, layers: Int) {
+    repeat(layers) {
+        repeat(candles + 2) {
+            print("@")
+        }
+        println()
+    }
+}
